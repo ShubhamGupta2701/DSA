1496586bd22041f25a01f72d531bdb26395b3802
Shubham Gupta <shubham272101@gmail.com>
2024-06-03 22:18:57 +0530

    add information for lec1

--- HashMaps/Lec1.cpp
+++ HashMaps/Lec1.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 int main(){
     int arr[10];
-    int hashmap[10] = {0,0,0,0,0,0,0,0,0,0};
+    int hashmap[10] = {0,0,0,0,0,0,0,0,0,0};   // use this to take input from user
     for(int i=0;i<10;i++){
         cin>>arr[i];
     }
-    for(int i = 0;i <10;i++){
+    for(int i = 0;i <10;i++){  // use this to calculate frequency of every element
         hashmap[arr[i]]++; 
     }
 
-    for(int i=0;i<10;i++){
+    for(int i=0;i<10;i++){  // use to show output
         cout<< i << ":"<<hashmap[i]<<endl;
     }
 
